Combitech/codefarm

commit 33d55652197b58361923d9b26f8b269d599b73e1

UI: PagedList now gets find direction and value to relate to from prop
Route path is not used anymore. Instead code, artifact and job lists
have state variables linked to the address location. This state variables
is given as property relative to PagedList.

src/app/UI/client/components/type_admin/PagedList.js
@@ -5,14 +5,11 @@ import LoadIndicator from "./LoadIndicator";
 import ListComponentItem from "./ListItem";
 import ListComponent from "./ListComponent";
 import { Button } from "react-toolbox/lib/button";
-import { ListDivider } from "react-toolbox/lib/list";
 import { filterFields as qbFilterFields } from "ui-lib/query_builder";
 
 const END_MARK_HEAD = "__HEAD__";
 const END_MARK_TAIL = "__TAIL__";
-const isEndMarkPath = (path) => path.includes("/page/") && (
-    (path.endsWith(END_MARK_HEAD) || path.endsWith(END_MARK_TAIL))
-);
+const isEndMark = (value) => value === END_MARK_HEAD || value === END_MARK_TAIL;
 
 const TURN_PAGE_KIND = {
     NEXT: "NEXT",
@@ -21,9 +18,14 @@ const TURN_PAGE_KIND = {
     LAST: "LAST"
 };
 
+const isFromRelative = (relative) => relative === END_MARK_HEAD || relative.startsWith(">");
+const isToRelative = (relative) => relative === END_MARK_TAIL || relative.startsWith("<");
+const encodeRelative = (value, isFrom = null) =>
+    `${typeof isFrom !== "boolean" ? "" : (isFrom ? ">" : "<")}${value}`;
+
 class PagedListComponent extends Component {
     constructor(props) {
-        super(props, true);
+        super(props);
 
         this.addStateVariable("nextPageHasMoreData", false);
 
@@ -34,9 +36,9 @@ class PagedListComponent extends Component {
                 pageSize: props.pageSize,
                 sortOn: props.sortOn,
                 sortOnType: props.sortOnType,
-                relativeValue: (props.pathname.includes("/page/") && !isEndMarkPath(props.pathname))
-                    ? props.pathname.split("/").pop() : null,
-                sortDesc: props.pathname.includes("/page/from/"),
+                relativeValue: !isEndMark(props.relative.value)
+                    ? props.relative.value.substr(1) : null,
+                sortDesc: isFromRelative(props.relative.value),
                 hasMoreDataCb: (nextPageHasMoreData) => this.state.nextPageHasMoreData.set(nextPageHasMoreData)
             }),
             (props) => {
@@ -52,54 +54,33 @@ class PagedListComponent extends Component {
     }
 
     turnPage(turnKind) {
-        const dirFromTurnKind = {
-            [ TURN_PAGE_KIND.NEXT ]: "from",
-            [ TURN_PAGE_KIND.FIRST ]: "from",
-            [ TURN_PAGE_KIND.PREV ]: "to",
-            [ TURN_PAGE_KIND.LAST ]: "to"
-        };
-        const direction = dirFromTurnKind[turnKind];
-        let basePath = this.props.pathname;
-        const removeStart = basePath.indexOf("/page/");
-        if (removeStart !== -1) {
-            basePath = basePath.slice(0, removeStart);
-        }
-        const wasLastTurnPrev = this.props.pathname.includes("/page/to/");
+        const isFrom = turnKind === TURN_PAGE_KIND.NEXT || turnKind === TURN_PAGE_KIND.FIRST;
         let itemIdx;
-        let value;
+        let value = encodeRelative(isFrom ? END_MARK_HEAD : END_MARK_TAIL);
         if (turnKind === TURN_PAGE_KIND.NEXT || turnKind === TURN_PAGE_KIND.PREV) {
-            if (direction === "from") {
+            const wasLastTurnPrev = isToRelative(this.props.relative.value);
+            if (isFrom) {
                 itemIdx = wasLastTurnPrev ? 0 : this.state.list.length - 1;
             } else {
                 itemIdx = wasLastTurnPrev ? this.state.list.length - 1 : 0;
             }
             const relativeToItem = this.state.list[itemIdx];
-            value = relativeToItem[this.props.sortOn];
-        } else {
-            value = direction === "from" ? END_MARK_HEAD : END_MARK_TAIL;
+            value = encodeRelative(relativeToItem[this.props.sortOn], isFrom);
         }
-        const newPath = `${basePath}/page/${direction}/${value}`;
-        console.log(`TURN ${newPath} - ${turnKind} - ${direction}`);
-        this.context.router.push({
-            pathname: newPath
-        });
+        this.props.relative.set(value);
     }
 
     componentDidUpdate() {
         // If we find that we have navigated to head or tail, change
         // route so that we follow head or tail
         const nextHasNoData = this.state.nextPageHasMoreData.value === false;
-        if (nextHasNoData && !isEndMarkPath(this.props.pathname)) {
-            if (this.props.pathname.includes("/page/from/")) {
+        if (nextHasNoData && !isEndMark(this.props.relative.value)) {
+            if (isFromRelative(this.props.relative.value)) {
                 // We are at end, follow tail
-                this.context.router.push({
-                    pathname: this.props.pathname.replace(/\/page\/from\/[^\/]*/, `/page/to/${END_MARK_TAIL}`)
-                });
-            } else if (this.props.pathname.includes("/page/to/")) {
+                this.props.relative.set(encodeRelative(END_MARK_TAIL));
+            } else {
                 // We are at start, follow head
-                this.context.router.push({
-                    pathname: this.props.pathname.replace(/\/page\/to\/[^\/]*/, `/page/from/${END_MARK_HEAD}`)
-                });
+                this.props.relative.set(encodeRelative(END_MARK_HEAD));
             }
         }
     }
@@ -121,15 +102,22 @@ class PagedListComponent extends Component {
             );
         }
 
-        const nextPageHasNoData = this.props.pathname.includes("/page/from/") && this.state.nextPageHasMoreData.value === false;
-        const prevPageHasNoData = this.props.pathname.includes("/page/to/") && this.state.nextPageHasMoreData.value === false;
-        if ((nextPageHasNoData || prevPageHasNoData) && !isEndMarkPath(this.props.pathname)) {
+        const relative = this.props.relative.value;
+        const isRelativeFrom = isFromRelative(relative);
+        const noMoreData = this.state.nextPageHasMoreData.value === false;
+
+        const nextPageHasNoData = isRelativeFrom && noMoreData;
+        const prevPageHasNoData = !isRelativeFrom && noMoreData;
+        if ((nextPageHasNoData || prevPageHasNoData) && !isEndMark(relative)) {
             // Skip rendering since componentDidUpdate will trigger a re-render anyway
             return;
         }
 
+        const isFirstPage = isRelativeFrom && relative === END_MARK_HEAD;
+        const isLastPage = !isRelativeFrom && relative === END_MARK_TAIL;
+
         let list = this.state.list;
-        if (list && this.props.pathname.includes("/page/to/")) {
+        if (list && !isRelativeFrom) {
             list = list.slice(0).reverse();
         }
 
@@ -137,6 +125,7 @@ class PagedListComponent extends Component {
             <div>
                 <this.props.ListComponent
                     theme={this.props.theme}
+                    listContext={this.props.listContext}
                     children={list && list.map((item) => (
                         <this.props.ListItemComponent
                             key={item._id}
@@ -150,13 +139,13 @@ class PagedListComponent extends Component {
                 <div>
                     <Button
                         icon="first_page"
-                        disabled={this.props.pathname.includes(`/page/from/${END_MARK_HEAD}`)}
+                        disabled={isFirstPage}
                         onClick={() => this.turnPage(TURN_PAGE_KIND.FIRST)}
                     />
                     <Button
                         icon="navigate_before"
                         disabled={list.length === 0 ||
-                            this.props.pathname.includes(`/page/from/${END_MARK_HEAD}`) ||
+                            isFirstPage ||
                             prevPageHasNoData
                         }
                         onClick={() => this.turnPage(TURN_PAGE_KIND.PREV)}
@@ -164,14 +153,14 @@ class PagedListComponent extends Component {
                     <Button
                         icon="navigate_next"
                         disabled={list.length === 0 ||
-                            this.props.pathname.includes(`/to/${END_MARK_TAIL}`) ||
+                            isLastPage ||
                             nextPageHasNoData
                         }
                         onClick={() => this.turnPage(TURN_PAGE_KIND.NEXT)}
                     />
                     <Button
                         icon="last_page"
-                        disabled={this.props.pathname.includes(`/to/${END_MARK_TAIL}`)}
+                        disabled={isLastPage}
                         onClick={() => this.turnPage(TURN_PAGE_KIND.LAST)}
                     />
                 </div>
@@ -198,15 +187,21 @@ PagedListComponent.propTypes = {
     query: React.PropTypes.object,
     onSelect: React.PropTypes.func,
     ListComponent: React.PropTypes.func.isRequired,
+    listContext: React.PropTypes.any,
     ListItemComponent: React.PropTypes.func.isRequired,
     listItemContext: React.PropTypes.any,
     pageSize: React.PropTypes.number,
     sortOn: React.PropTypes.string,
     sortOnType: React.PropTypes.string,
-    pathname: React.PropTypes.string.isRequired
+    relative: React.PropTypes.object.isRequired,
+    // Required by state_var when linkToLocation is used
+    location: React.PropTypes.object.isRequired,
+    // Required by state_var when linkToLocation is used
+    route: React.PropTypes.object.isRequired
 };
 
 PagedListComponent.contextTypes = {
+    // Required by state_var when linkToLocation is used
     router: React.PropTypes.object.isRequired
 };
 

src/app/UI/client/components/type_admin/View.js
@@ -52,7 +52,9 @@ class View extends Component {
             breadcrumbs: this.props.breadcrumbs,
             controls: [],
             pathname: this.getPathname(),
-            context: this.props.context || this.state.context
+            context: this.props.context || this.state.context,
+            location: this.props.location,
+            route: this.props.route
         };
 
         if (!props.item) {
@@ -153,6 +155,7 @@ View.propTypes = {
     item: React.PropTypes.object,
     params: React.PropTypes.object.isRequired,
     route: React.PropTypes.object.isRequired,
+    location: React.PropTypes.object.isRequired,
     children: React.PropTypes.node,
     breadcrumbs: React.PropTypes.array,
     context: React.PropTypes.object

src/app/UI/client/lib/state_var.js
@@ -107,16 +107,16 @@ const stateVar = (inst, propName, initialValue) => {
         toggle: () => varInst.set(!inst.state[propName].value),
         linkToLocation: (replace = false) => {
             const newVarInst = varInst.set(inst.props.location.query[propName]);
-            newVarInst.router = inst.props.router;
+            newVarInst.router = inst.context.router;
             newVarInst.route = inst.props.route;
             newVarInst.replace = replace;
 
-            newVarInst.route.onChange = (prevState, nextState) => {
+            newVarInst.onRouteChange = (prevState, nextState) => {
                 const newState = {};
-
                 for (const key of Object.keys(inst.state)) {
                     if (typeof inst.state[key] === "object" && inst.state[key].router) {
-                        const newStateVar = inst.state[key].create(nextState.location.query[key]);
+                        const queryParamVal = nextState.location.query[key];
+                        const newStateVar = inst.state[key].create(queryParamVal);
 
                         if (newStateVar !== inst.state[key]) {
                             newState[key] = newStateVar;
@@ -128,9 +128,32 @@ const stateVar = (inst, propName, initialValue) => {
                     inst.setState(newState);
                 }
             };
+            /* React router route.onChange only allows for a single listener.
+             * Add property onChangeHandlers to route and install onChange listener
+             * that triggers all handlers on onChangeHandlers
+             */
+            newVarInst.route.onChangeHandlers = newVarInst.route.onChangeHandlers || [];
+            newVarInst.route.onChangeHandlers.push(newVarInst.onRouteChange);
+            newVarInst.route.onChange = (prevState, nextState) =>
+                newVarInst.route.onChangeHandlers.forEach((handler) => handler(prevState, nextState));
         },
         unlinkFromLocation: () => {
-            varInst.onChange = null;
+            if (varInst.onRouteChange) {
+                // Handler exists, remove from onChangeHandlers
+                if (varInst.route && varInst.route.onChangeHandlers) {
+                    const removeIdx = varInst.route.onChangeHandlers.indexOf(varInst.onRouteChange);
+                    if (removeIdx !== -1) {
+                        varInst.route.onChangeHandlers.splice(removeIdx, 1);
+                    }
+                }
+                varInst.onRouteChange = null;
+            }
+            if (varInst.route && varInst.route.onChangeHandlers) {
+                if (varInst.route.onChangeHandlers.length === 0) {
+                    varInst.route.onChangeHandlers = null;
+                    varInst.route.onChange = null;
+                }
+            }
             delete varInst.router;
             delete varInst.route;
         },

src/app/UI/lib/managers/administration/client/components/sections/job/List.js
@@ -13,6 +13,7 @@ class List extends Component {
         super(props);
 
         this.addStateVariable("filter", "");
+        this.addStateVariable("relative", "__HEAD__", true);
     }
 
     render() {
@@ -43,12 +44,14 @@ class List extends Component {
                     filterFields={[ "name", "status", "criteria" ]}
                     pageSize={5}
                     ListItemComponent={JobListItem}
-                    pathname={this.props.pathname}
                     onSelect={(item) => {
                         this.context.router.push({
                             pathname: `${this.props.pathname}/${item._id}`
                         });
                     }}
+                    location={this.props.location}
+                    route={this.props.route}
+                    relative={this.state.relative}
                 />
             </TASection>
         );
@@ -60,7 +63,9 @@ List.propTypes = {
     pathname: React.PropTypes.string.isRequired,
     type: React.PropTypes.string.isRequired,
     breadcrumbs: React.PropTypes.array.isRequired,
-    controls: React.PropTypes.array.isRequired
+    controls: React.PropTypes.array.isRequired,
+    location: React.PropTypes.object.isRequired,
+    route: React.PropTypes.object.isRequired
 };
 
 List.contextTypes = {

src/app/UI/lib/managers/administration/client/components/sections/job/routes.js
@@ -15,6 +15,48 @@ import Remove from "./Remove";
 const routes = (
     <Route
         path="jobs"
+        component={TAView}
+        List={List}
+        type="exec.job"
+        label="Jobs"
+    >
+        <Route
+            path="create"
+            component={TAEdit}
+            Create={Edit}
+            type="exec.job"
+        />
+        <Route
+            path=":_id"
+            component={TAView}
+            Item={JobItem}
+            type="exec.job"
+        >
+            {/* <Route
+                path="edit"
+                component={TAEdit}
+                Edit={Edit}
+            />*/}
+            <Route
+                path="remove"
+                component={TARemove}
+                Remove={Remove}
+            />
+            <Route
+                path=":subJobId"
+                component={TAView}
+                Item={SubJobItem}
+                type="exec.subjob"
+            />
+        </Route>
+    </Route>
+);
+
+/*
+
+const routes = (
+    <Route
+        path="jobs"
         label="Jobs"
     >
         <IndexRedirect to="page/from/__HEAD__" />
@@ -48,11 +90,6 @@ const routes = (
             Item={JobItem}
             type="exec.job"
         >
-            {/* <Route
-                path="edit"
-                component={TAEdit}
-                Edit={Edit}
-            />*/}
             <Route
                 path="remove"
                 component={TARemove}
@@ -67,5 +104,5 @@ const routes = (
         </Route>
     </Route>
 );
-
+*/
 export default routes;

src/app/UI/lib/managers/artifacts/client/components/List.js
@@ -13,6 +13,7 @@ class List extends Component {
         super(props);
 
         this.addStateVariable("filter", "");
+        this.addStateVariable("relative", "__HEAD__", true);
     }
 
     render() {
@@ -42,14 +43,16 @@ class List extends Component {
                     query={{ repository: this.props.item._id }}
                     filter={this.state.filter.value}
                     filterFields={[ "name", "version", "state", "repository" ]}
-                    pageSize={8}
+                    pageSize={10}
                     ListItemComponent={ArtifactListItem}
-                    pathname={this.props.pathname}
                     onSelect={(item) => {
                         this.context.router.push({
                             pathname: `${this.props.pathname}/${item._id}`
                         });
                     }}
+                    location={this.props.location}
+                    route={this.props.route}
+                    relative={this.state.relative}
                 />
             </TASection>
         );
@@ -61,7 +64,9 @@ List.propTypes = {
     item: React.PropTypes.object,
     pathname: React.PropTypes.string.isRequired,
     breadcrumbs: React.PropTypes.array.isRequired,
-    controls: React.PropTypes.array.isRequired
+    controls: React.PropTypes.array.isRequired,
+    location: React.PropTypes.object.isRequired,
+    route: React.PropTypes.object.isRequired
 };
 
 List.contextTypes = {

commit db909cfb72fa6566776079026d63d659492bd21f

UI: Fix bug introduced in 33d5565 where child routes wasn't loaded correctly

src/app/UI/client/components/type_admin/PagedList.js
@@ -195,8 +195,6 @@ PagedListComponent.propTypes = {
     sortOnType: React.PropTypes.string,
     relative: React.PropTypes.object.isRequired,
     // Required by state_var when linkToLocation is used
-    location: React.PropTypes.object.isRequired,
-    // Required by state_var when linkToLocation is used
     route: React.PropTypes.object.isRequired
 };
 

src/app/UI/client/components/type_admin/View.js
@@ -52,19 +52,17 @@ class View extends Component {
             breadcrumbs: this.props.breadcrumbs,
             controls: [],
             pathname: this.getPathname(),
-            context: this.props.context || this.state.context,
-            location: this.props.location,
-            route: this.props.route
+            context: this.props.context || this.state.context
         };
 
-        if (!props.item) {
+        if (props.item) {
             props.breadcrumbs = props.breadcrumbs.concat({
-                label: this.props.route.label,
+                label: props.item._id,
                 pathname: this.getPathname()
             });
         } else {
             props.breadcrumbs = props.breadcrumbs.concat({
-                label: props.item._id,
+                label: this.props.route.label,
                 pathname: this.getPathname()
             });
         }
@@ -117,7 +115,11 @@ class View extends Component {
 
             if (this.props.route.Item) {
                 return (
-                    <this.props.route.Item {...props} />
+                    <this.props.route.Item
+                        {...props}
+                        route={this.props.route}
+                        type={this.props.route.type}
+                    />
                 );
             }
         }
@@ -137,6 +139,7 @@ class View extends Component {
             <div className={this.props.theme.view}>
                 {this.props.route.List && <this.props.route.List
                     {...props}
+                    route={this.props.route}
                     type={this.props.route.type}
                 />}
             </div>

src/app/UI/client/lib/state_var.js
@@ -106,8 +106,9 @@ const stateVar = (inst, propName, initialValue) => {
         },
         toggle: () => varInst.set(!inst.state[propName].value),
         linkToLocation: (replace = false) => {
-            const newVarInst = varInst.set(inst.props.location.query[propName]);
-            newVarInst.router = inst.context.router;
+            const router = inst.context.router;
+            const newVarInst = varInst.set(router.location.query[propName]);
+            newVarInst.router = router;
             newVarInst.route = inst.props.route;
             newVarInst.replace = replace;
 
@@ -134,8 +135,10 @@ const stateVar = (inst, propName, initialValue) => {
              */
             newVarInst.route.onChangeHandlers = newVarInst.route.onChangeHandlers || [];
             newVarInst.route.onChangeHandlers.push(newVarInst.onRouteChange);
-            newVarInst.route.onChange = (prevState, nextState) =>
-                newVarInst.route.onChangeHandlers.forEach((handler) => handler(prevState, nextState));
+            const onChangeHandler = (route, prevState, nextState) =>
+                route.onChangeHandlers &&
+                route.onChangeHandlers.forEach((handler) => handler(prevState, nextState));
+            newVarInst.route.onChange = onChangeHandler.bind(null, newVarInst.route);
         },
         unlinkFromLocation: () => {
             if (varInst.onRouteChange) {
@@ -146,11 +149,15 @@ const stateVar = (inst, propName, initialValue) => {
                         varInst.route.onChangeHandlers.splice(removeIdx, 1);
                     }
                 }
-                varInst.onRouteChange = null;
+                delete varInst.onRouteChange;
             }
-            if (varInst.route && varInst.route.onChangeHandlers) {
-                if (varInst.route.onChangeHandlers.length === 0) {
-                    varInst.route.onChangeHandlers = null;
+            if (varInst.route) {
+                if (varInst.route.onChangeHandlers) {
+                    if (varInst.route.onChangeHandlers.length === 0) {
+                        delete varInst.route.onChangeHandlers;
+                        varInst.route.onChange = null;
+                    }
+                } else {
                     varInst.route.onChange = null;
                 }
             }

src/app/UI/lib/managers/administration/client/components/sections/job/List.js
@@ -49,7 +49,6 @@ class List extends Component {
                             pathname: `${this.props.pathname}/${item._id}`
                         });
                     }}
-                    location={this.props.location}
                     route={this.props.route}
                     relative={this.state.relative}
                 />
@@ -64,7 +63,6 @@ List.propTypes = {
     type: React.PropTypes.string.isRequired,
     breadcrumbs: React.PropTypes.array.isRequired,
     controls: React.PropTypes.array.isRequired,
-    location: React.PropTypes.object.isRequired,
     route: React.PropTypes.object.isRequired
 };
 

src/app/UI/lib/managers/administration/client/components/sections/job/routes.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Route, Redirect, IndexRedirect } from "react-router";
+import { Route } from "react-router";
 import {
     View as TAView,
     Edit as TAEdit,
@@ -52,57 +52,4 @@ const routes = (
     </Route>
 );
 
-/*
-
-const routes = (
-    <Route
-        path="jobs"
-        label="Jobs"
-    >
-        <IndexRedirect to="page/from/__HEAD__" />
-        <Route
-            path="page/from/:id"
-            component={TAView}
-            List={List}
-            type="exec.job"
-            label="Jobs"
-        >
-            <Redirect from=":_id" to="/admin/jobs/:_id" />
-        </Route>
-        <Route
-            path="page/to/:id"
-            component={TAView}
-            List={List}
-            type="exec.job"
-            label="Jobs"
-        >
-            <Redirect from=":_id" to="/admin/jobs/:_id" />
-        </Route>
-        <Route
-            path="create"
-            component={TAEdit}
-            Create={Edit}
-            type="exec.job"
-        />
-        <Route
-            path=":_id"
-            component={TAView}
-            Item={JobItem}
-            type="exec.job"
-        >
-            <Route
-                path="remove"
-                component={TARemove}
-                Remove={Remove}
-            />
-            <Route
-                path=":subJobId"
-                component={TAView}
-                Item={SubJobItem}
-                type="exec.subjob"
-            />
-        </Route>
-    </Route>
-);
-*/
 export default routes;

src/app/UI/lib/managers/artifacts/client/components/List.js
@@ -50,7 +50,6 @@ class List extends Component {
                             pathname: `${this.props.pathname}/${item._id}`
                         });
                     }}
-                    location={this.props.location}
                     route={this.props.route}
                     relative={this.state.relative}
                 />
@@ -65,7 +64,6 @@ List.propTypes = {
     pathname: React.PropTypes.string.isRequired,
     breadcrumbs: React.PropTypes.array.isRequired,
     controls: React.PropTypes.array.isRequired,
-    location: React.PropTypes.object.isRequired,
     route: React.PropTypes.object.isRequired
 };
 

src/app/UI/lib/managers/artifacts/client/components/Page.js
@@ -10,7 +10,7 @@ const REPO_TYPE = "artifactrepo.repository";
 
 class Page extends Component {
     constructor(props) {
-        super(props, true);
+        super(props);
 
         this.addTypeListStateVariable("list", REPO_TYPE, {}, true);
     }

src/app/UI/lib/managers/code/client/components/List.js
@@ -181,7 +181,6 @@ class RevisionList extends Component {
                         pathname: `${this.props.pathname}/${item._id}`
                     });
                 }}
-                location={this.props.location}
                 route={this.props.route}
                 relative={this.props.relative}
             />
@@ -194,9 +193,8 @@ RevisionList.propTypes = {
     query: React.PropTypes.object,
     filter: React.PropTypes.string,
     pathname: React.PropTypes.string.isRequired,
-    location: React.PropTypes.object.isRequired,
     route: React.PropTypes.object.isRequired,
-    relative: React.PropTypes.object.isRequired,
+    relative: React.PropTypes.object.isRequired
 };
 
 RevisionList.contextTypes = {
@@ -263,7 +261,6 @@ class RevisionTabs extends Component {
                                     }}
                                     filter={this.state.filter.value}
                                     pathname={this.props.pathname}
-                                    location={this.props.location}
                                     route={this.props.route}
                                     relative={this.state.submittedRel}
                                 />
@@ -277,7 +274,6 @@ class RevisionTabs extends Component {
                                     }}
                                     filter={this.state.filter.value}
                                     pathname={this.props.pathname}
-                                    location={this.props.location}
                                     route={this.props.route}
                                     relative={this.state.mergedRel}
                                 />
@@ -296,7 +292,6 @@ RevisionTabs.propTypes = {
     pathname: React.PropTypes.string.isRequired,
     breadcrumbs: React.PropTypes.array.isRequired,
     controls: React.PropTypes.array.isRequired,
-    location: React.PropTypes.object.isRequired,
     route: React.PropTypes.object.isRequired
 };
 