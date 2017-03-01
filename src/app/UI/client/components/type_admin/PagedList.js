2f0156c916a4dc4d663f61aa2d1cb526b6df6ee9
Marcus Christensson <marcus.christensson@combitech.se>
2017-03-01 09:36:54 +0100

    UI: Convert TAPagedList to LightComponent

--- src/app/UI/client/components/type_admin/PagedList.js
+++ src/app/UI/client/components/type_admin/PagedList.js
@@ -1,86 +1,69 @@
 
 import React from "react";
-import Component from "ui-lib/component";
+import LightComponent from "ui-lib/light_component";
 import LoadIndicator from "./LoadIndicator";
 import ListComponentItem from "./ListItem";
 import ListComponent from "./ListComponent";
-import { Button } from "react-toolbox/lib/button";
-import { filterFields as qbFilterFields } from "ui-lib/query_builder";
+import ListPager from "./ListPager";
+import PagedTypeList from "ui-observables/paged_type_list";
+import { States as ObservableDataStates } from "ui-lib/observable_data";
 
-const END_MARK_HEAD = "__HEAD__";
-const END_MARK_TAIL = "__TAIL__";
-const isEndMark = (value) => value === END_MARK_HEAD || value === END_MARK_TAIL;
-
-const TURN_PAGE_KIND = {
-    NEXT: "NEXT",
-    PREV: "PREV",
-    FIRST: "FIRST",
-    LAST: "LAST"
-};
-
-const isFromRelative = (relative) => relative === END_MARK_HEAD || relative.startsWith(">");
-const isToRelative = (relative) => relative === END_MARK_TAIL || relative.startsWith("<");
-const encodeRelative = (value, isFrom = null) =>
-    `${typeof isFrom !== "boolean" ? "" : (isFrom ? ">" : "<")}${value}`;
-
-class PagedListComponent extends Component {
+class PagedListComponent extends LightComponent {
     constructor(props) {
         super(props);
 
-        this.addStateVariable("nextPageHasMoreData", false);
+        this.typeList = new PagedTypeList({
+            type: props.type,
+            query: props.query,
+            sortOn: props.sortOn,
+            sortOnType: props.sortOnType,
+            sortDesc: props.sortDesc,
+            limit: props.limit,
+            filter: props.filter,
+            filterFields: props.filterFields
+        });
 
-        this.addTypePagedListStateVariable(
-            "list",
-            (props) => props.type,
-            (props) => ({
-                pageSize: props.pageSize,
-                sortOn: props.sortOn,
-                sortOnType: props.sortOnType,
-                relativeValue: !isEndMark(props.relative.value)
-                    ? props.relative.value.substr(1) : null,
-                sortDesc: isFromRelative(props.relative.value),
-                hasMoreDataCb: (nextPageHasMoreData) => this.state.nextPageHasMoreData.set(nextPageHasMoreData)
-            }),
-            (props) => {
-                let filterQuery = {};
-                if (props.filterFields.length > 0 && props.filter && props.filter.length > 0) {
-                    filterQuery = qbFilterFields(props.filterFields, props.filter, "si");
-                }
-
-                return Object.assign({}, props.query, filterQuery);
-            },
-            true
-        );
+        this.state = {
+            list: this.typeList.value.getValue(),
+            state: this.typeList.state.getValue(),
+            error: this.typeList.error.getValue()
+        };
     }
 
-    turnPage(turnKind) {
-        const isFrom = turnKind === TURN_PAGE_KIND.NEXT || turnKind === TURN_PAGE_KIND.FIRST;
-        let itemIdx;
-        let value = encodeRelative(isFrom ? END_MARK_HEAD : END_MARK_TAIL);
-        if (turnKind === TURN_PAGE_KIND.NEXT || turnKind === TURN_PAGE_KIND.PREV) {
-            const wasLastTurnPrev = isToRelative(this.props.relative.value);
-            if (isFrom) {
-                itemIdx = wasLastTurnPrev ? 0 : this.state.list.length - 1;
-            } else {
-                itemIdx = wasLastTurnPrev ? this.state.list.length - 1 : 0;
-            }
-            const relativeToItem = this.state.list[itemIdx];
-            value = encodeRelative(relativeToItem[this.props.sortOn], isFrom);
-        }
-        this.props.relative.set(value);
+    componentWillReceiveProps(nextProps) {
+        this.log("componentWillReceiveProps", nextProps);
+        this.typeList.setOpts({
+            type: nextProps.type,
+            query: nextProps.query,
+            sortOn: nextProps.sortOn,
+            sortOnType: nextProps.sortOnType,
+            sortDesc: nextProps.sortDesc,
+            limit: nextProps.limit,
+            filter: nextProps.filter,
+            filterFields: nextProps.filterFields
+        });
+    }
+
+    componentDidMount() {
+        this.log("componentDidMount");
+        this.addDisposable(this.typeList.start());
+        this.addDisposable(this.typeList.value.subscribe((list) => this.setState({ list })));
+        this.addDisposable(this.typeList.state.subscribe((state) => this.setState({ state })));
+        this.addDisposable(this.typeList.error.subscribe((error) => this.setState({ error })));
     }
 
     componentDidUpdate() {
-        // If we find that we have navigated to head or tail, change
-        // route so that we follow head or tail
-        const nextHasNoData = this.state.nextPageHasMoreData.value === false;
-        if (nextHasNoData && !isEndMark(this.props.relative.value)) {
-            if (isFromRelative(this.props.relative.value)) {
-                // We are at end, follow tail
-                this.props.relative.set(encodeRelative(END_MARK_TAIL));
-            } else {
-                // We are at start, follow head
-                this.props.relative.set(encodeRelative(END_MARK_HEAD));
+        /* If relative paging is used and current page has no next or previous
+         * page, then automatically navigate to last or first page.
+         */
+        const pagingInfo = this.typeList.pagingInfo.getValue().toJS();
+        if (pagingInfo.isRelative) {
+            if (!pagingInfo.hasNextPage) {
+                this.log("setLastPage");
+                this.typeList.setLastPage();
+            } else if (!pagingInfo.hasPrevPage) {
+                this.log("setFirstPage");
+                this.typeList.setFirstPage();
             }
         }
     }
@@ -88,37 +71,16 @@ class PagedListComponent extends Component {
     render() {
         this.log("render", this.props, this.state);
 
-        if (this.state.errorAsync.value) {
+        if (this.state.error) {
             return (
-                <div>{this.state.errorAsync.value}</div>
+                <pre>{this.state.error.toJS()}</pre>
             );
         }
 
-        if (this.state.loadingAsync.value) {
+        if (this.state.state === ObservableDataStates.LOADING) {
             return (
-                <LoadIndicator
-                    theme={this.props.theme}
-                />
+                <LoadIndicator theme={this.props.theme}/>
             );
-        }
-
-        const relative = this.props.relative.value;
-        const isRelativeFrom = isFromRelative(relative);
-        const noMoreData = this.state.nextPageHasMoreData.value === false;
-
-        const nextPageHasNoData = isRelativeFrom && noMoreData;
-        const prevPageHasNoData = !isRelativeFrom && noMoreData;
-        if ((nextPageHasNoData || prevPageHasNoData) && !isEndMark(relative)) {
-            // Skip rendering since componentDidUpdate will trigger a re-render anyway
-            return;
-        }
-
-        const isFirstPage = isRelativeFrom && relative === END_MARK_HEAD;
-        const isLastPage = !isRelativeFrom && relative === END_MARK_TAIL;
-
-        let list = this.state.list;
-        if (list && !isRelativeFrom) {
-            list = list.slice(0).reverse();
         }
 
         return (
@@ -126,7 +88,7 @@ class PagedListComponent extends Component {
                 <this.props.ListComponent
                     theme={this.props.theme}
                     listContext={this.props.listContext}
-                    children={list && list.map((item) => (
+                    children={this.state.list && this.state.list.toJS().map((item) => (
                         <this.props.ListItemComponent
                             key={item._id}
                             theme={this.props.theme}
@@ -136,34 +98,13 @@ class PagedListComponent extends Component {
                         />
                     ))}
                 />
-                <div>
-                    <Button
-                        icon="first_page"
-                        disabled={isFirstPage}
-                        onClick={() => this.turnPage(TURN_PAGE_KIND.FIRST)}
+                {this.state.list && this.state.list.size > 0 &&
+                    <ListPager
+                        theme={this.props.theme}
+                        pagedList={this.typeList}
+                        pagingInfo={this.typeList.pagingInfo.getValue()}
                     />
-                    <Button
-                        icon="navigate_before"
-                        disabled={list.length === 0 ||
-                            isFirstPage ||
-                            prevPageHasNoData
-                        }
-                        onClick={() => this.turnPage(TURN_PAGE_KIND.PREV)}
-                    />
-                    <Button
-                        icon="navigate_next"
-                        disabled={list.length === 0 ||
-                            isLastPage ||
-                            nextPageHasNoData
-                        }
-                        onClick={() => this.turnPage(TURN_PAGE_KIND.NEXT)}
-                    />
-                    <Button
-                        icon="last_page"
-                        disabled={isLastPage}
-                        onClick={() => this.turnPage(TURN_PAGE_KIND.LAST)}
-                    />
-                </div>
+                }
             </div>
         );
     }
@@ -173,9 +114,11 @@ PagedListComponent.defaultProps = {
     ListComponent: ListComponent,
     ListItemComponent: ListComponentItem,
     query: {},
-    pageSize: 5,
+    limit: 5,
     sortOn: "created",
     sortOnType: "Date",
+    sortDesc: true,
+    filter: "",
     filterFields: []
 };
 
@@ -190,12 +133,10 @@ PagedListComponent.propTypes = {
     listContext: React.PropTypes.any,
     ListItemComponent: React.PropTypes.func.isRequired,
     listItemContext: React.PropTypes.any,
-    pageSize: React.PropTypes.number,
+    limit: React.PropTypes.number,
     sortOn: React.PropTypes.string,
     sortOnType: React.PropTypes.string,
-    relative: React.PropTypes.object.isRequired,
-    // Required by state_var when linkToLocation is used
-    route: React.PropTypes.object.isRequired
+    sortDesc: React.PropTypes.bool
 };
 
 PagedListComponent.contextTypes = {
