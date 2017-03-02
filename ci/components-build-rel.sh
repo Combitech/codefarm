8761b87aba702988c455bd4449c1935068683b7a
patrikfirek <patrik.firek@gmail.com>
2017-03-02 12:55:06 +0100

    CI scripts (#104)

---
+++ ci/components-build-rel.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+function printUsage() {
+  echo "Usage: $0 [components] or 'all'"
+}
+
+if [ $# -lt 1 ]; then
+  echo "Error: Illegal number of arguments"
+  printUsage
+  exit 1
+fi
+
+targets=$1
+
+gitroot=$(git rev-parse --show-toplevel)
+source $gitroot/ci/common.source
+
+if [ "${targets}" == "all" ]; then
+  targets=${components[@]}
+else
+  for target in ${targets[@]}; do
+    if [[ " ${components[*]} " != *" ${target} "* ]]; then
+      echo "Error: Component must be any number of ${components[*]} or 'all'"
+      printUsage
+      exit 1
+    fi
+  done
+fi
+
+for target in ${targets[@]}; do
+  ${gitroot}/ci/component-build.sh ${target} rel
+done
