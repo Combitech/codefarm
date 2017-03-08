f081e8eed326582a4aa1e68bfdfc715e227d4ab0
Mattias Runge <mattias@runge.se>
2017-03-08 17:08:57 +0100

    testsupport: Fix for lint error

--- src/lib/testsupport/lib/mocha_patch.js
+++ src/lib/testsupport/lib/mocha_patch.js
@@ -6,10 +6,10 @@ module.exports = () => {
     if (it) {
         const orgIt = it;
 
-        it = function(name, fn) {
+        it = function(name, fn) { // eslint-disable-line no-global-assign
             orgIt(name, function() {
-                return fn.bind(this)(this);
+                return fn.bind(this)(this); // eslint-disable-line no-invalid-this
             });
-        }
+        };
     }
 };
