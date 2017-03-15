e116610ff9dd63a605b8ea877a61588672bde957
Marcus Christensson <marcus.christensson@combitech.se>
2017-03-15 10:16:08 +0100

    UI: Use TAForm for signin/signout
    Cleanup of auth manager client parts.

--- src/app/UI/lib/managers/auth/client/components/Page.js
+++ src/app/UI/lib/managers/auth/client/components/Page.js
@@ -1,110 +1,21 @@
 import React from "react";
 import LightComponent from "ui-lib/light_component";
-import { Card, CardTitle, CardText, CardActions } from "react-toolbox/lib/card";
-import { Input } from "react-toolbox/lib/input";
-import { Button } from "react-toolbox/lib/button";
-import { Row, Col } from "react-flexbox-grid";
-import { signin, signout } from "ui-lib/auth";
-import ActiveUser from "ui-observables/active_user";
+import SignInOutForm from "./SignInOutForm";
 
 class Page extends LightComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeUser: ActiveUser.instance.user.getValue(),
-            emailOrId: "",
-            password: ""
-        };
-    }
-
-    componentDidMount() {
-        this.addDisposable(ActiveUser.instance.user.subscribe((activeUser) => this.setState({ activeUser })));
-    }
-
-    async _login() {
-        const response = await signin(this.state.emailOrId, this.state.password);
-        this.log("Sign in response", response);
-        if (response.success) {
-            this.context.router.push({
-                pathname: "/"
-            });
-        }
-    }
-
-    async _logout() {
-        const response = await signout();
-        this.log("Sign out response", response);
-        if (response.success) {
-            this.context.router.push({
-                pathname: "/signin"
-            });
-        }
-    }
-
     render() {
         this.log("render", this.props);
 
         return (
             <div className={this.props.theme.content}>
-                <Row>
-                    <Col xs={12} mdOffset={3} md={6} lgOffset={4} lg={4}>
-                        <Card>
-                            {this.state.activeUser.get("userLoggedIn") ? (
-                                <div>
-                                    <CardTitle title="CodeFarm Sign Out"/>
-                                    <CardText>
-                                        User {this.state.activeUser.get("username")} currently signed in.
-                                    </CardText>
-                                    <CardActions>
-                                        <Button
-                                            label="Sign out"
-                                            onClick={() => this._logout()}
-                                        />
-                                    </CardActions>
-                                </div>
-                            ) : (
-                                <div>
-                                    <CardTitle title="CodeFarm Sign In"/>
-                                    <CardText>
-                                        <Input
-                                            type="text"
-                                            label="User id or email"
-                                            required={true}
-                                            value={this.state.emailOrId}
-                                            onChange={(emailOrId) => this.setState({ emailOrId })}
-                                        />
-                                        <Input
-                                            type="password"
-                                            label="Password"
-                                            required={true}
-                                            value={this.state.password}
-                                            onChange={(password) => this.setState({ password })}
-                                        />
-                                    </CardText>
-                                    <CardActions>
-                                        <Button
-                                            label="Sign in"
-                                            onClick={() => this._login()}
-                                        />
-                                    </CardActions>
-                                </div>
-                            )}
-                        </Card>
-                    </Col>
-                </Row>
+                <SignInOutForm theme={this.props.theme} />
             </div>
         );
     }
 }
 
 Page.propTypes = {
-    theme: React.PropTypes.object,
-    children: React.PropTypes.node,
-    route: React.PropTypes.object.isRequired
-};
-
-Page.contextTypes = {
-    router: React.PropTypes.object.isRequired
+    theme: React.PropTypes.object
 };
 
 export default Page;
--- src/app/UI/lib/managers/auth/client/components/routes.js
+++ src/app/UI/lib/managers/auth/client/components/routes.js
@@ -2,15 +2,8 @@
 import React from "react";
 import { Route } from "react-router";
 import Page from ".";
-import ProfilePage from "./ProfilePage";
 
 const routes = [
-    // TODO: Move profile elsewhere...
-    <Route
-        key="profile"
-        path="/profile"
-        component={ProfilePage}
-    />,
     <Route
         key="signin"
         path="/signin"
