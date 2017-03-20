c8a653fec5f7c79d171fa04d0aa3eeccf3f3beb4
patrikfirek <patrik.firek@gmail.com>
2017-03-20 11:49:11 +0100

    Coderepo: Initial test code, dummy test SKIP_REVIEW (#193)

---
+++ src/app/CodeRepo/test/github_backend.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/* global describe it before after */
+
+const { assert } = require("chai");
+const { mochaPatch } = require("testsupport");
+
+mochaPatch();
+
+describe("GithubBackend", () => {
+    before(async () => {
+    });
+
+    after(async () => {
+    });
+
+    describe("Setup repo", async () => {
+        it("", async () => {
+            assert(true);
+        });
+    });
+});
