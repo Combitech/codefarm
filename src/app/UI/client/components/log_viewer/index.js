5c5ad17287c4133f396c1b4a20da52f6d3ca8aea
Mattias Runge <mattias.runge@combitech.se>
2017-03-21 09:39:45 +0100

    UI: Updated LogCard to show log lines only when expanded

--- src/app/UI/client/components/log_viewer/index.js
+++ src/app/UI/client/components/log_viewer/index.js
@@ -4,10 +4,13 @@ import { LOG_VIEWER } from "../identifiers.js";
 import theme from "./theme.scss";
 
 import LogViewer from "./LogViewer.js";
+import LogLines from "./LogLines.js";
 
 const ThemedLogViewer = themr(LOG_VIEWER, theme)(LogViewer);
+const ThemedLogLines = themr(LOG_VIEWER, theme)(LogLines);
 
 export default ThemedLogViewer;
 export {
-    ThemedLogViewer as LogViewer
+    ThemedLogViewer as LogViewer,
+    ThemedLogLines as LogLines
 };
