26cc4f5ceee00f0e6301262222dcb08acea326e4
patrikfirek <patrik.firek@gmail.com>
2017-03-30 14:05:43 +0200

    rest_stub in testsupport (SKIP_REVIEW) (#229)

--- src/lib/testsupport/index.js
+++ src/lib/testsupport/index.js
@@ -4,5 +4,6 @@ const captureStream = require("./lib/capture_stream");
 const delay = require("./lib/delay");
 const pollUntil = require("./lib/poll_until");
 const mochaPatch = require("./lib/mocha_patch");
+const RestStub = require("./lib/rest_stub");
 
-module.exports = { captureStream, delay, pollUntil, mochaPatch };
+module.exports = { captureStream, delay, pollUntil, mochaPatch, RestStub };
