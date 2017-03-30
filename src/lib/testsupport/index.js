4a79b223665de171bf362215300c42e2cb096eb9
Patrik Firek <patrik.firek@combitech.se>
2017-03-30 14:08:10 +0200

    Added servicecom_stub embryo to testsupport

--- src/lib/testsupport/index.js
+++ src/lib/testsupport/index.js
@@ -5,5 +5,6 @@ const delay = require("./lib/delay");
 const pollUntil = require("./lib/poll_until");
 const mochaPatch = require("./lib/mocha_patch");
 const RestStub = require("./lib/rest_stub");
+const serviceComStub = require("./lib/servicecom_stub");
 
-module.exports = { captureStream, delay, pollUntil, mochaPatch, RestStub };
+module.exports = { captureStream, delay, pollUntil, mochaPatch, RestStub, serviceComStub };
